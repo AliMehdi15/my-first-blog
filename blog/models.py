67874f6ead8c2cb62777b408e04ef8051204da96
Mehdi <ali_mehdi_1996@yahoo.com>
2019-07-20 19:53:47 +0530

    edited the error of view post

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
-
-		
-		
+	
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -23,8 +20,7 @@ class Post(models.Model):
         return self.title
 	
 #def approved_comments(self):
-#	    return self.comments.filter(approved_comment=True)  
-		
+#	    return self.comments.filter(approved_comment=True)  	
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
@@ -37,8 +33,4 @@ class Comment(models.Model):
         self.save()
 
     def __str__(self):
-        return self.text
-		
-
-		
-	
+        return self.text	
